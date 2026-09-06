e578c9bf89557406f16fc307efcf114ee1ba3fc5
Julia Nguyen <julia@uxj.io>
2026-09-06 12:44:29 -0400

    fix: stabilize X3 EPUB anti-aliasing (#3400)
    Co-Authored-By: Alfred Bot <Gersh.alfred@gmail.com>

--- lib/hal/HalDisplay.cpp
+++ lib/hal/HalDisplay.cpp
@@ -77,6 +77,8 @@ void HalDisplay::waitRefreshComplete() { einkDisplay.waitRefreshComplete(); }
 
 bool HalDisplay::supportsAsyncRefresh() const { return einkDisplay.supportsAsyncRefresh(); }
 
+bool HalDisplay::supportsAsyncGrayscaleBase() const { return !gpio.deviceIsX3() && einkDisplay.supportsAsyncRefresh(); }
+
 void HalDisplay::refreshDisplay(HalDisplay::RefreshMode mode, bool turnOffScreen) {
   if (gpio.deviceIsX3() && mode == RefreshMode::HALF_REFRESH) {
     einkDisplay.requestResync(1);
